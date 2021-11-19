0e59a1a57aad04a9fdd2005c3258d3b418be7796
justnuggets <jnguyen0749@gmail.com>
2021-11-19 14:53:26 -0800

    Mark Anim for Replacement
    Mark ANIM_GFX_WEB to be replaced with new object

--- data/player_names.asm
+++ data/player_names.asm
@@ -10,10 +10,10 @@ ChrisNameMenuHeader:
 	db 5 ; items
 	db "NEW NAME@"
 MalePlayerNameArray:
-	db "CHRIS@"
-	db "MAT@"
-	db "ALLAN@"
-	db "JON@"
+	db "TRISTAN@"
+	db "EMRIC@"
+	db "FADEN@"
+	db "MEMPHIS@"
 	db 2 ; title indent
 	db " NAME @" ; title
 
@@ -29,9 +29,9 @@ KrisNameMenuHeader:
 	db 5 ; items
 	db "NEW NAME@"
 FemalePlayerNameArray:
-	db "KRIS@"
-	db "AMANDA@"
-	db "JUANA@"
-	db "JODI@"
+	db "TRISTA@"
+	db "EMILY@"
+	db "FAYE@"
+	db "MORIEL@"
 	db 2 ; title indent
 	db " NAME @" ; title
